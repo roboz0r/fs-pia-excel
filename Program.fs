d8d456bf5a3579529c73e6abd489465f35e357f8
Rob Lenders <mental.blindfolds.on@gmail.com>
2024-03-06 22:49:03 +0200

    demo usage of Excel from F#

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿open Microsoft.Office.Interop.Excel
+
+let app = new ApplicationClass()
+let wb = app.Workbooks.Add()
+
+(wb.ActiveSheet :?> Worksheet).Cells.[1,1] <- "Hello from F#"
+
+wb.SaveAs(@"C:\Users\Robert\Desktop\test.xlsx")
+wb.Close()
+app.Quit()
